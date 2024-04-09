22af0b4f54982dc155380e8f8ffa8b2a64473c92
rgyarko <Rachameyawameyaw@gmail.com>
2024-04-08 20:25:52 -0400

    restructured my files and folders

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+// const db = require('./db/db.json');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes  = require("./routes/htmlRoutes");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Use Router
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// Delete note ...
+
+// Server initialization...
+app.listen(PORT, () => {
+  console.log(`Notetaking occurring on port ${PORT}`);
+});
